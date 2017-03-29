3df0e210812a79ff3bb5136f6d2a00aafb8919d9
wqs <wqshappy2008@126.com>
2017-03-29 17:47:16 +0800

    new

---
+++ pages/index/index.js
@@ -0,0 +1,33 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: '你好我的小程序',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  OnShareAppMessage:function(){
+    return {
+      title:'我的好伙伴',
+      desc:'最全的小助手',
+      path:'pages/index?id=123'
+    }
+  }
+})
